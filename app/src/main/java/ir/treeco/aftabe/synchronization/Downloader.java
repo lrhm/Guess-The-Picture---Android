08f534cfba7177fd6f707a5c86d14a81d077cb0c
behdad222 <behdad.222@gmail.com>
2015-05-22 18:51:44 +0430

    download & save yml file

--- app/src/main/java/ir/treeco/aftabe/synchronization/Downloader.java
+++ app/src/main/java/ir/treeco/aftabe/synchronization/Downloader.java
@@ -1,24 +1,28 @@
 package ir.treeco.aftabe.synchronization;
 
-import android.content.Context;
+import android.util.Log;
 
 import com.thin.downloadmanager.DownloadStatusListener;
 
 public class Downloader implements DownloadStatusListener {
 
-    public Downloader(String bookServerID, Context context, String destinationUri, String userId, String encMD5, String nameBook) {
+    public Downloader() {
     }
+    //todo check she agar file bashe moshkel pish nayad
 
     @Override
     public void onDownloadComplete(int id) {
+        Log.e("behdad downloader", "donloade complate");
     }
 
     @Override
     public void onDownloadFailed(int id, int errorCode, String errorMessage) {
+        Log.e("behdad downloader", "donloade faild");
     }
 
     @Override
     public void onProgress(int id, long totalBytes, int progress) {
+        Log.e("behdad downloader", "donloading");
 
     }
 }
--- app/src/main/java/ir/treeco/aftabe/synchronization/Sync.java
+++ app/src/main/java/ir/treeco/aftabe/synchronization/Sync.java
@@ -3,13 +3,17 @@ package ir.treeco.aftabe.synchronization;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
+import android.util.Log;
 
 import com.thin.downloadmanager.DownloadManager;
 import com.thin.downloadmanager.DownloadRequest;
 import com.thin.downloadmanager.ThinDownloadManager;
 
+import java.io.File;
 
-/**
+
+/*
  * Created by behdad on 5/22/15.
  */
 
@@ -21,17 +25,26 @@ public class Sync extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
+        Log.e("behdad", "recive net");
 
         downloadManager = new ThinDownloadManager(DOWNLOAD_THREAD_POOL_SIZE);
 
+        Uri downloadUri = Uri.parse("http://rsdn.ir/files/aftabe.yml");
+        downloader = new Downloader();
 
-        downloader = new Downloader(bookServerID, context, String.valueOf(destinationUri), user.getServerId(), encryptedMD5, product.getName());
+
+
+        File appDir = context.getDir ("data", Context.MODE_PRIVATE);
+        //Getting a file within the dir.
+        File downloadPath = new File (appDir, "data.yml");
+
+        Log.e("bahdad" , "download path " + String.valueOf(downloadPath));
 
         final DownloadRequest downloadRequest = new DownloadRequest(downloadUri)
-                .setDestinationURI(destinationUri).setPriority(DownloadRequest.Priority.NORMAL)
+                .setDestinationURI(Uri.fromFile(downloadPath)).setPriority(DownloadRequest.Priority.NORMAL)
                 .setDownloadListener(downloader);
 
-        if (downloadManager.query(1) == DownloadManager.STATUS_NOT_FOUND) { //todo query
+        if (downloadManager.query(1) == DownloadManager.STATUS_NOT_FOUND) {
             downloadManager.add(downloadRequest);
         }
     }
