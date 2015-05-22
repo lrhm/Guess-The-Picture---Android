fd34a93023dba84466d6e3b068771257f0dcb644
behdad222 <behdad.222@gmail.com>
2015-05-22 18:19:42 +0430

    add downloader in sync

--- app/src/main/java/ir/treeco/aftabe/synchronization/Sync.java
+++ app/src/main/java/ir/treeco/aftabe/synchronization/Sync.java
@@ -4,12 +4,35 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
+import com.thin.downloadmanager.DownloadManager;
+import com.thin.downloadmanager.DownloadRequest;
+import com.thin.downloadmanager.ThinDownloadManager;
+
+
 /**
  * Created by behdad on 5/22/15.
  */
+
 public class Sync extends BroadcastReceiver {
+
+    private ThinDownloadManager downloadManager;
+    private static final int DOWNLOAD_THREAD_POOL_SIZE = 4;
+    Downloader downloader;
+
     @Override
     public void onReceive(Context context, Intent intent) {
 
+        downloadManager = new ThinDownloadManager(DOWNLOAD_THREAD_POOL_SIZE);
+
+
+        downloader = new Downloader(bookServerID, context, String.valueOf(destinationUri), user.getServerId(), encryptedMD5, product.getName());
+
+        final DownloadRequest downloadRequest = new DownloadRequest(downloadUri)
+                .setDestinationURI(destinationUri).setPriority(DownloadRequest.Priority.NORMAL)
+                .setDownloadListener(downloader);
+
+        if (downloadManager.query(1) == DownloadManager.STATUS_NOT_FOUND) { //todo query
+            downloadManager.add(downloadRequest);
+        }
     }
 }
