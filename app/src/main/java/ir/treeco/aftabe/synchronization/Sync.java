d4de153466eaa4e1668217cd48a8ac2831736091
behdad222 <behdad.222@gmail.com>
2015-05-22 17:41:45 +0430

    update sdk version

---
+++ app/src/main/java/ir/treeco/aftabe/synchronization/Sync.java
@@ -0,0 +1,15 @@
+package ir.treeco.aftabe.synchronization;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by behdad on 5/22/15.
+ */
+public class Sync extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
