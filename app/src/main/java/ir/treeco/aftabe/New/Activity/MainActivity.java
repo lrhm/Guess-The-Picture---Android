0fc4dd6a89c644998c7408a886389c052caa59e8
behdad222 <behdad.222@gmail.com>
2015-05-28 19:32:04 +0430

    new main add

---
+++ app/src/main/java/ir/treeco/aftabe/New/Activity/MainActivity.java
@@ -0,0 +1,35 @@
+package ir.treeco.aftabe.New.Activity;
+
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.ViewPager;
+
+import com.ogaclejapan.smarttablayout.SmartTabLayout;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItemAdapter;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItems;
+
+import ir.treeco.aftabe.New.Fragment.PackageFragmentNew;
+import ir.treeco.aftabe.R;
+
+public class MainActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.new_main_activity);
+
+        FragmentPagerItemAdapter adapter = new FragmentPagerItemAdapter(
+                getSupportFragmentManager(), FragmentPagerItems.with(this)
+                .add("تازه‌ها", PackageFragmentNew.class)
+                .add("دانلود شده‌ها", PackageFragmentNew.class)
+                .add("محبوب‌ترین‌ها", PackageFragmentNew.class)
+                .create());
+
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+        viewPager.setAdapter(adapter);
+
+        SmartTabLayout viewPagerTab = (SmartTabLayout) findViewById(R.id.viewpagertab);
+        viewPagerTab.setViewPager(viewPager);
+    }
+}
---
+++ app/src/main/java/ir/treeco/aftabe/New/Fragment/PackageFragmentNew.java
@@ -0,0 +1,19 @@
+package ir.treeco.aftabe.New.Fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import ir.treeco.aftabe.R;
+
+public class PackageFragmentNew extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.new_fragment_package, container, false);
+    }
+}
